15b65f6a604c4f869455870fad7319694b694e64
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2026-01-06 16:21:58 +0700

    Initial Commit

---
+++ SpiritPath/SpiritPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiritPathApp.swift
+//  SpiritPath
+//
+//  Created by punyapath on 6/1/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct SpiritPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
